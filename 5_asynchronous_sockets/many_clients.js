e28eff9bc7254a5813fa98ef3fdd0e6cb501bb1a
Dave McKenna <davemckenna01@gmail.com>
2014-11-22 22:40:36 -0500

    5. Asynchronous TCP sockets for nonblocking server
    This server users asynchronous sockets to accept connections and read
    from and write to sockets. This is non-blocking I/O, because the main process
    does not wait for a read or write to finish before performing other tasks,
    such as reading and writing to other sockets.

---
+++ 5_asynchronous_sockets/many_clients.js
@@ -0,0 +1,31 @@
+var net = require('net');
+ 
+for (var i = 0; i < 4; i++) {
+    sendStuff(new net.Socket(), '1', 10);
+}
+
+function sendStuff(client, character, size) {
+    client.connect(8080, '127.0.0.1', function() {
+        var str = '';
+
+        console.log('Connected...');
+
+        for (var i = 0; i < size; i++) {
+            str += character; 
+        }
+
+        str += '\n\n';
+
+        client.write(str);
+    });
+
+    client.on('data', function(data) {
+        console.log('Received: ' + data);
+        client.destroy(); // kill client after server's response
+    });
+     
+    client.on('close', function() {
+        console.log('Connection closed');
+    });
+}
+
